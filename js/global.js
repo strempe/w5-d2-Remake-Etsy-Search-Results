1ddbfe22313aa1c2ab04b888aaf2e8aaf66b28c0
Sara Trempe <88.trempe.sara@gmail.com>
2017-04-26 12:01:37 -0400

    refactored Etsy search

--- js/global.js
+++ js/global.js
@@ -1,39 +1,47 @@
-var games = { };
-var searchButton = document.querySelector('#search');
+var etsy
+getData();
 
-getVintageBoardGames()
+document.querySelector('#searchInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        getData();
+    }
+});
 
-function getVintageBoardGames() {
+document.querySelector('#searchBtn').addEventListener('click', getData);
 
-    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('board games') + '&includes=Images,Shop')
-        .then(response => response.json())
-        .then(data => {
-            games = data.results;
-            getResults(games);      
 
-            console.log(games);    
-            document.querySelector('.form-control').value
-        })      
+function getData() {
+    var searchInput = document.querySelector('#searchInput').value;
+    document.querySelector('.itemCards').innerHTML = '';
+
+    if (searchInput !== '') {
+        fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent(searchInput) + '&includes=Images,Shop')
+            .then(response => response.json())
+            .then(data => {
+                etsy = data.results;
+                renderItemCard(etsy);
+                // console.log(etsy);
+            })
+    }
 }
 
-searchButton.addEventListener('click');
-
-function getResults(games) {
-    games.forEach(function(info){
-    var card = `<div class="card col-sm-4 col-md-3">
-                    <img src="${info.Images[0].url_170x135}"/>
-                    <h5 class="cardCaption">${info.title.slice(0, 50)}...</h5>
-            </div>
+function renderItemCard(items) {
+    // console.log(items)
+    items.forEach(function (details) {
+        var card = `<div class="col-sm-4  col-md-3">
+            <div class="card">
+            <img src="${details.Images[0].url_170x135}"/>
+            <h5 class="card-caption">${details.title.slice(0, 50)}...</h5>
             <div class="row">
-                    <h6 class="cardSeller col-sm-6">${info.Shop.shop_name}</h6>
-                    <h6 class="price col-sm-6">${info.price}</h6>
-            </div>`;
+                <h6 class="card-author col-sm-6">${details.Shop.shop_name}</h6>
+                <h6 class="card-price text-right col-sm-6">${details.price}</h6>
+            </div>
+            </div>
+        </div>`;
 
-    document.querySelector('#results').innerHTML += card;
-
-    })  
-}
-
+        document.querySelector('.itemCards').innerHTML += card;
+    })
+};
 
 
 
